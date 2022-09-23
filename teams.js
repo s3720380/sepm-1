79890b57e963261e42b4584f84073c93d6f1db4f
Fisseha Abate 3850910 <s3850910@student.rmit.edu.au>
2022-09-23 20:00:36 +1000

    added file upload

--- teams.js
+++ teams.js
@@ -1,3 +1,11 @@
-function msg(){  
-    alert("Placeholder!");  
-   }  
+const uploadconfirm = document.getElementById('uploadconfirm').addEventListener('click', () => {
+    Papa.parse(document.getElementById('uploadfile').files[0],
+        {
+            download: true,
+            complete: function (results) {
+                str = JSON.stringify(results);
+                document.getElementById('output').innerHTML = str;
+                console.log(results);
+            }
+        })
+})
